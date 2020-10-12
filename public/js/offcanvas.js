a7738070fee61e5724a27fcb24f23a168d24912f
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2020-10-11 18:28:36 -0700

    Show Edit New pages styled

--- public/js/offcanvas.js
+++ public/js/offcanvas.js
@@ -5,3 +5,14 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
 })
+// $(function () {
+//   $('[data-toggle="popover"]').popover()
+// });
+
+$(document).ready(function() {
+  $(".dropdown-menu").css('margin','50px');
+  $(function () {
+      $('[data-toggle="popover"]').popover()
+  })
+  $("#popoverData").popover({ trigger: "hover" });
+});
