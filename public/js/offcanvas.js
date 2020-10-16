d1cf908af681543d2e2d4daea80bf805b0817978
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2020-10-16 13:10:08 -0700

    spinning hamburger

--- public/js/offcanvas.js
+++ public/js/offcanvas.js
@@ -1,3 +1,10 @@
+const hamburger = document.querySelector('.navbar-toggler');
+
+hamburger.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+
 $(function () {
   'use strict'
 
