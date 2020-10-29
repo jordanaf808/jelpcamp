57e02cc4614aac3b9abe690403b582c7eff159b4
Jordan Aceves-Foster <69128716+jordanaf808@users.noreply.github.com>
2020-10-29 13:27:44 -0700

    add js menu

--- public/js/offcanvas.js
+++ public/js/offcanvas.js
@@ -12,14 +12,3 @@ $(function () {
     $('.offcanvas-collapse').toggleClass('open')
   })
 })
-// $(function () {
-//   $('[data-toggle="popover"]').popover()
-// });
-
-$(document).ready(function() {
-  $(".dropdown-menu").css('margin','50px');
-  $(function () {
-      $('[data-toggle="popover"]').popover()
-  })
-  $("#popoverData").popover({ trigger: "hover" });
-});
